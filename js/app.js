f264479a951636f0e361bcb7e1c9e9d8a78cf190
sjones79 <jonessm@umich.edu>
2016-04-10 21:11:10 -0400

    Remove some of the api based logic to new apiService

--- js/app.js
+++ js/app.js
@@ -1,71 +1,91 @@
 'use strict';
 
-var app = function () {
+/*
+var YTGallery = {
     
-    var loadMovieTrailers = function(callback) { 
     
-        var jsonFile = "data/movie_trailers.json";
-        var xobj = new XMLHttpRequest();
+    
+}
+
+
+click on a nav item,
+    the first movie for that category shows up as the main movie
+    then clicking on any of the other trailers for that category makes it the main one.
+
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var app = function () {
+        
+        var comicBookMenu = document.querySelector('#comicbooks');
+        var martialArtMenu = document.querySelector('#martialarts');
+        var animeMenu = document.querySelector('#anime');
+        
+        
+        comicBookMenu.addEventListener('click', function(e){
+           // document.getElementById('movie-title').innerHTML = "Movies From Comic Books";
+            loadMovieTrailersFromFile(getComicBookMovies);
+        });
+        
+
+        /*
+        var loadMovieTrailersFromFile = function(callback) { 
+    
+            var jsonFile = "data/movie_trailers.json";
+            var xobj = new XMLHttpRequest();
             xobj.overrideMimeType("application/json");
-        xobj.open('GET', jsonFile, true); // Replace 'my_data' with the path to your file
-        xobj.onreadystatechange = function () {
-              if (xobj.readyState == 4 && xobj.status == "200") {
-                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-                callback(xobj.responseText);
-              }
-        };
-        xobj.send(null);  
-     }
+            xobj.open('GET', jsonFile, true); // Replace 'my_data' with the path to your file
+            xobj.onreadystatechange = function () {
+              
+                if (xobj.readyState == 4 && xobj.status == "200") {
+                    // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                    callback(xobj.responseText);
+                }
+            };
+            xobj.send(null);  
+        }
+    
+        var movieTrailers = loadMovieTrailersFromFile(function(response){
+            var stuff = JSON.parse(response);
+            console.log(stuff);
+            return stuff;
+
+        });  */
   
-    //OMDB is an open source api for movie information
-    var omdbRequest = function(movieTitle) {
-        var movieRequest = new XMLHttpRequest();
-        var url = "http://www.omdbapi.com/?t="+movieTitle+"&y=&plot=short&r=json";
+        //OMDB is an open source api for movie information
+        var omdbRequest = function(movieTitle) {
+            var omdbRequest = new XMLHttpRequest();
+            var url = "http://www.omdbapi.com/?t="+movieTitle+"&y=&plot=short&r=json";
 
-        movieRequest.onreadystatechange = function() {
-            if (movieRequest.readyState == 4 && movieRequest.status == 200) {
-                var movieResponse = JSON.parse(movieRequest.responseText);
-                setMovieData(movieResponse);
+            omdbRequest.onreadystatechange = function() {
+                if (omdbRequest.readyState == 4 && omdbRequest.status == 200) {
+                    var movieResponse = JSON.parse(omdbRequest.responseText);
+                    setOMDBData(movieResponse);
+                }
+            };
+            omdbRequest.open("GET", url, true);
+            omdbRequest.send();
+        }
+    
+    
+        function setOMDBData(movieData) {
+          /*  var out = "";
+            var i;
+            for(i = 0; i < arr.length; i++) {
+                out += '<a href="' + arr[i].url + '">' + 
+                arr[i].display + '</a><br>';
             }
-        };
-        movieRequest.open("GET", url, true);
-        movieRequest.send();
+            document.getElementById("id01").innerHTML = out; */
+            console.log(movieData);
+
+        }
+
+        omdbRequest("Fist of the North Star");   
+    
     }
-    
-    var movieTrailers = loadMovieTrailers(function(response){
-        var stuff = JSON.parse(response);
-        console.log(stuff);
-        return stuff;
-        //console.log("inside callback of load movie trailer");
-        //console.log(movieTrailerInfo);
-    });
-    
-    console.log("movieTrailers",movieTrailers);
 
-function setMovieData(movieData) {
-  /*  var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' + 
-        arr[i].display + '</a><br>';
-    }
-    document.getElementById("id01").innerHTML = out; */
-    console.log(movieData);
-
-}
-
-
-
-
-
-
-
-
-
-omdbRequest("Fist of the North Star");   
-    
-}
-
-app();
+    app();
+});
 
 
