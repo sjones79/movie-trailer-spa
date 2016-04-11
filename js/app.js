1fac327b53b956f6cf6a283b879abf7d47fa4464
sjones79 <jonessm@umich.edu>
2016-04-11 01:41:54 -0400

    Remove unneeded code

--- js/app.js
+++ js/app.js
@@ -9,36 +9,38 @@ click on a nav item,
 */
 
 document.addEventListener('DOMContentLoaded', function () {
-
-    var app = function () {
         
         var comicBookMenu = document.querySelector('#comicbooks');
         var martialArtMenu = document.querySelector('#martialarts');
         var animeMenu = document.querySelector('#anime');
         
-        
+           //by default load the comic book movies
+        document.getElementById('movie-category').innerHTML = "Comic Book Movies";
+        loadMovieTrailersFromFile(getDefaultMovie);
+       
+    
         comicBookMenu.addEventListener('click', function(e){
-            document.getElementById('movie-title').innerHTML = "Comic Book API Service Call";
+            document.getElementById('movie-category').innerHTML = "Comic Book API Service Call";
             loadMovieTrailersFromFile(getComicBookMovies);
         });
         
         martialArtMenu.addEventListener('click', function(e){
-            document.getElementById('movie-title').innerHTML = "Martial Arts API Service Call";
+            document.getElementById('movie-category').innerHTML = "Martial Arts API Service Call";
             loadMovieTrailersFromFile(getMartialArtsMovies);
         });
         
         animeMenu.addEventListener('click', function(e){
-            document.getElementById('movie-title').innerHTML = "Anime API Service Call";
+            document.getElementById('movie-category').innerHTML = "Anime API Service Call";
             loadMovieTrailersFromFile(getAnimeMovies);
         });
-        
-        function displayData (movie, movieCategoryObj)  {
-            document.getElementById('movie-description').innerHTML = '<p>' + movie.Plot + '</p>';
-        }
     
-    }
-
-    app();
+       
+        
 });
+
+var displayData = function (movieResponse) {
+    document.getElementById('movie-title').innerHTML = movieResponse.Title;
+    document.getElementById('movie-description').innerHTML = '<p>' + movieResponse.Plot +'</p>';
+}
 
 
