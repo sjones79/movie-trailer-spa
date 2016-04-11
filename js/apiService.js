467f141c1d31b830ac019ec90c26c9c92cd9a39c
sjones79 <jonessm@umich.edu>
2016-04-11 16:37:55 -0400

    Error handling remove youtube player code

--- js/apiService.js
+++ js/apiService.js
@@ -10,45 +10,53 @@
 //TODO make it clear what the callbacks are
 
 var currentMovieObj = {};
-var loadMovieTrailersFromFile = function(callback, category) { 
-    
-    //TODO check if callback is not null and is a function before proceeding
-    var jsonFile = "data/movie_trailers.json";
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', jsonFile, true); 
-    xobj.onreadystatechange = function () {
 
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText, category);
-        }
-    };
-    xobj.send(null);  
+var loadMovieTrailersFromFile = function(category) { 
+
+    if(category != null){
+         var jsonFile = "data/movie_trailers.json";
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', jsonFile, true); 
+        xobj.onreadystatechange = function () {
+
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                getMovieDataByCategory(xobj.responseText, category);
+            }
+        };
+        xobj.send(null);  
+    } else {
+          console.log("Error unable to load movie trailers from file");  
+    }
 }
 
 var getMovieDataByCategory = function (data, category) {
     var movieData = {};
     var selectedImdbId;
-    var youtubeId;
-    JSON.parse(data).forEach(function(movieObj){
-        if( movieObj.category.toLowerCase() === category.toLowerCase()){
-            movieData = movieObj;
-        }
-    });
+    var ytId;
     
-    selectedImdbId = movieData["movies"][0].imdb_id;
-    youtubeId = movieData["movies"][0].yt_id;
-    currentMovieObj = movieData;
-    getMovieDataFromOmdb(movieData,selectedImdbId, youtubeId);
+    if(data != null && category != null ){
+        JSON.parse(data).forEach(function(movieObj){
+            if( movieObj.category.toLowerCase() === category.toLowerCase()){
+                movieData = movieObj;
+            }
+        });
     
+        selectedImdbId = movieData["movies"][0].imdb_id;
+        ytId = movieData["movies"][0].yt_id;
+        currentMovieObj = movieData;
+        getMovieDataFromOmdb(movieData,selectedImdbId, ytId);
+    } else {
+          console.log("Error unable to get movie data by category");  
+    }
 }
 
 
 //takes one movie object from a given category to fetch data from
 var getMovieDataFromOmdb = function (movieObj, imdbId, ytId) {
+    //TODO verify all parameters before using them
     //pass the current movie object and the already selected movie id
     updatePreviewList(movieObj, imdbId);
-    setYTPlayer(ytId);
     makeOMDBRequest(null, imdbId, displayData);
 }
 
@@ -84,55 +92,9 @@ var makeOMDBRequest = function (movieTitle, imdbId, callback) {
 }
 
 var getSelectedMovieByImdbId = function (imdbId, ytId) {
-   getMovieDataFromOmdb(currentMovieObj, imdbId, ytId)
-}
-
-
-var setYTPlayer = function (videoId) {
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-            
-    var player;
-    
-    window.onYouTubeIframeAPIReady = function() {
-        player = new YT.Player('player', {
-            height: '500',
-            width: '750',
-            videoId: videoId,
-            playerVars: {
-                html5: 1,
-                controls: 0,
-                modestbranding: 0,
-                showinfo: 0,
-                rel: 0
-            },
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-          });
-     }
-
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-            stopVideo();
-            done = true;
-        }
-    }
-    function stopVideo() {
-        player.stopVideo();
+    if(imdbId != null && ytId != null) {
+        getMovieDataFromOmdb(currentMovieObj, imdbId, ytId);
     }
 }
+
+
